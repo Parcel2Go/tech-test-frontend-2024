59195f55b813850a70cc885c05bd80a0b57d9dce
Ben Warham <b.warham@parcel2go.com>
2024-04-17 18:39:08 +0100

    Display Reliability rating

--- src/components/QuoteResult.tsx
+++ src/components/QuoteResult.tsx
@@ -1,13 +1,16 @@
-import { Grid } from '@mui/material';
+import { Grid, Rating } from '@mui/material';
 import Price from './Price';
 
 export default function QuoteResult({ quote }: { quote: Quote }) {
   return (
     <Grid container spacing={2}>
-      <Grid item xs={8}>
+      <Grid item xs={6}>
         <h3>{quote.service.name}</h3>
       </Grid>
-      <Grid item xs={4}>
+      <Grid item xs={3}>
+        <Rating value={quote.metadata['Rating:Reliability']} readOnly />
+      </Grid>
+      <Grid item xs={3}>
         <Price variant="body" price={quote.price.gross as number} />
       </Grid>
     </Grid>
